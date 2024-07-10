596d32b3a20ebb8027f22f397eeac1e69dacb773
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2024-07-10 11:44:23 +0200

    Correzione alla funzione di interpretazione dei polinomi, adesso i coefficienti sono ordinati dal più basso al più alto

--- polybrary/polynterpreter.m
+++ polybrary/polynterpreter.m
@@ -1,8 +1,8 @@
 function val = polynterpreter(coeff, x)
 arguments
-    coeff (1, :) double
-    x double
+    coeff (1,:) double {mustBeFloat ,mustBeNonmissing}
+    x (1,:) double {mustBeFloat ,mustBeNonmissing}
 end
 
-    val = flip(coeff) * (x' * ones(1, size(coeff, 2))).^(flip(0:size(coeff, 2) - 1))';
+    val = flip(coeff) * (x' * ones(1, size(coeff, 2))).^(0:size(coeff, 2) - 1)';
 end
