48eafdfc0f8531675782924350d8cddc9ca0791a
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2024-07-11 19:10:55 +0200

    Aggiunta una funzione per il calcolo dei punti di minimo / massimo / flesso di una funzione polinomiale

---
+++ polybrary/polystat.m
@@ -0,0 +1,36 @@
+% FINDER PER PUNTI STAZIONARI SU FUNZIONI POLINOMIALI
+% 
+% input:
+%       poly - coefficienti del polinomio in questione
+% 
+% output:
+%       max - vettore dei punti di massimo
+%       min - vettore dei punti di minimo
+%       inflexion - vettore dei punti di flesso
+function [max, min, inflexion] = polystat(poly)
+
+arguments
+    poly (1,:) double {mustBeNonmissing}
+end
+
+    der = polyder(poly);
+    der2 = polyder(der);
+
+    der_zeros = roots(der)';
+
+    max = inf(1, size(der_zeros, 2));
+    min = inf(1, size(der_zeros, 2));
+    inflexion = inf(1, size(der_zeros, 2));
+    
+    for i=1:size(der_zeros, 2)
+        % Punto di massimo
+        if (polyval(der2, der_zeros(i)) < 0)
+            max = [max der_zeros(i)];
+        % Punto di minimo
+        elseif (polyval(der2, der_zeros(i)) > 0)
+            min = [min der_zeros(i)];
+        else
+            inflexion = [inflexion der_zeros(i)];
+        end
+    end
+end
